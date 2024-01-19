d59683cc798981ba9e345e07dc80909edd957927
Thierry Arsicaud <t.arsicaud@catie.fr>
2024-01-19 17:58:19 +0100

    Reconfiguration pour tests

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-SAMPLING_RATE = 42     # in Hz
+SAMPLING_RATE = 46     # in Hz
 
 CSV_COLS_INDEX = {
     "t": 0,
@@ -12,3 +12,17 @@ CSV_COLS_INDEX = {
     "quaternion_y": 6,
     "quaternion_z": 7
 }
+
+
+
+# SAMPLING_RATE = 50     # in Hz
+
+# CSV_COLS_INDEX = {
+#     "raw_acceleration_x": 1,
+#     "raw_acceleration_y": 2,
+#     "raw_acceleration_z": 3,
+#     "quaternion_w": 10,
+#     "quaternion_x": 11,
+#     "quaternion_y": 12,
+#     "quaternion_z": 13,
+# }
