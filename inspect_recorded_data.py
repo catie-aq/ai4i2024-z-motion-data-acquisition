be48a5c54a05a514be51cccba785a0a0227197d8
Thierry Arsicaud <t.arsicaud@catie.fr>
2024-01-22 19:49:45 +0100

    Nouvelle implémentation pour calcul trajectoire

--- inspect_recorded_data.py
+++ inspect_recorded_data.py
@@ -12,38 +12,34 @@ from tools.kinematics import normalize_acceleration_data_quat, compute_velocity_
 from tools.filtering import butter_highpass_filter, filtfilt, zero_if_under_threshold
 from tools.visualization import plot_time_domain, plot_3d
 
-from conf import SAMPLING_RATE, CSV_COLS_INDEX
-
 
 def read_data_from(csv_file_path_name):
     data = pandas.read_csv(csv_file_path_name, delimiter=",", skiprows=0)
-    raw_acceleration_data = data.values[:,CSV_COLS_INDEX["raw_acceleration_x"]:CSV_COLS_INDEX["raw_acceleration_z"]+1]
-    quaternion_data = data.values[:,CSV_COLS_INDEX["quaternion_w"]:CSV_COLS_INDEX["quaternion_z"]+1]
-    return raw_acceleration_data, quaternion_data
-
+    return data
 
 def inspect_recorded_data(
-    csv_file_path_name,
-    sampling_rate
+    csv_file_path_name
     ):
     csv_file_name = os.path.basename(csv_file_path_name)
-    raw_acceleration_data, quaternion_data = read_data_from(csv_file_path_name)
+    data = read_data_from(csv_file_path_name)
+    t_data = data['t']
+    t_ref = data['t'].iloc[0]
+    data['t'] = data['t'] - t_ref
+
+    raw_acceleration_data = data[["raw_acceleration_x", "raw_acceleration_y", "raw_acceleration_z"]]
+    quaternion_data = data[["quaternion_w", "quaternion_x", "quaternion_y", "quaternion_z"]]
     normalized_acceleration_data = normalize_acceleration_data_quat(raw_acceleration_data, quaternion_data)
 
-    print(quaternion_data)
-
-
-    plot_time_domain(raw_acceleration_data, sampling_rate, "%s / raw acceleration" % csv_file_name)
-    plot_time_domain(normalized_acceleration_data, sampling_rate, "%s / normalized acceleration" % csv_file_name)
+    plt.plot(data['t'], raw_acceleration_data)
     plt.show()
 
-    # We may apply here some filters in order to enhance trajectory integration
+    plt.plot(data['t'], normalized_acceleration_data)
+    plt.show()
 
-    # normalized_acceleration_data = filtfilt(normalized_acceleration_data, 2, 1)
-    # normalized_acceleration_data = butter_highpass_filter(normalized_acceleration_data, 0.15 ,SAMPLING_RATE)
-    # normalized_acceleration_data = zero_if_under_threshold(normalized_acceleration_data, 1.0)
+    normalized_acceleration_data = filtfilt(normalized_acceleration_data, 2, 1)
+    normalized_acceleration_data = zero_if_under_threshold(normalized_acceleration_data, 1.0)
 
-    velocity_data, position_data = compute_velocity_position(raw_acceleration_data, quaternion_data, sampling_rate)
+    velocity_data, position_data = compute_velocity_position(raw_acceleration_data, quaternion_data, t_data)
     plot_3d(position_data, "%s / computed position" % csv_file_name)
     plt.show()
 
@@ -53,7 +49,7 @@ def main():
     parser.add_argument("csv_file_path", type=str, help="CSV file path")
     args = parser.parse_args()
     csv_file_path_name = args.csv_file_path
-    inspect_recorded_data(csv_file_path_name, SAMPLING_RATE)
+    inspect_recorded_data(csv_file_path_name)
 
 
 if __name__ == "__main__":
--- tools/kinematics.py
+++ tools/kinematics.py
@@ -1,78 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import pandas as pd
 import scipy
 
+
 from skinematics import quat, vector, rotmat
-
-
-def normalize_acceleration_data_irs(
-    initial_orientation_rotation_matrix,
-    raw_acceleration_data,
-    rotation_speed_data,
-    sampling_rate
-    ):
-    """
-    Computes normalized acceleration data from raw acceleration data
-    (including gravity) and rotation speed data.
-
-    * Based on code available on :
-    https://github.com/thomas-haslwanter/scikit-kinematics/blob/master/skinematics/imus.py
-
-    Parameters
-    ----------
-
-    initial_orientation_rotation_matrix : ndarray(3,3)
-        Rotation matrix describing the initial orientation of the sensor
-
-    raw_acceleration_data: ndarray(N,3)
-        Raw acceleration (including gravity), in [m/s^2]
-
-    rotation_speed_data: ndarray(N,3)
-        Angular velocity, in [rad/s]
-
-    sampling_rate: float
-        sampling rate, in [Hz]
-
-
-    Returns
-    ---------
-    Normalized acceleration data : ndarray(N,3)
-    Raises an exception otherwise.
-
-    """
-
-
-    # From :
-
-    # Transform recordings to angVel/acceleration in space --------------
-
-    # Orientation of \vec{g} with the sensor in the "initial_orientation_rotation_matrix"
-    # g = scipy.constants.g   # 9.80665
-    g = 9.81
-
-    g0 = np.linalg.inv(initial_orientation_rotation_matrix).dot(np.r_[0,0,g])
-
-    # for the remaining deviation, assume the shortest rotation to there
-    q0 = vector.q_shortest_rotation(raw_acceleration_data[0], g0)
-
-    q_initial = rotmat.convert(initial_orientation_rotation_matrix, to='quat')
-
-    # combine the two, to form a reference orientation. Note that the sequence
-    # is very important!
-    q_ref = quat.q_mult(q_initial, q0)
-
-    # Calculate orientation q by "integrating" rotation_speed_data -----------------
-    q = quat.calc_quat(rotation_speed_data, q_ref, sampling_rate, 'bf')
-
-    # Acceleration, velocity, and position ----------------------------
-    # From q and the measured acceleration, get the \frac{d^2x}{dt^2}
-    g_v = np.r_[0, 0, g]
-
-    linear_acceleration_data = raw_acceleration_data - vector.rotate_vector(g_v, quat.q_inv(q))
-    normalized_acceleration_data = vector.rotate_vector(linear_acceleration_data, q)
-
-    return normalized_acceleration_data
 
 
 
@@ -118,11 +51,14 @@ def normalize_acceleration_data_quat(
     return normalized_acceleration_data
 
 
+SAMPLING_RATE = 50
+
+
 
 def compute_velocity_position(
     raw_acceleration_data,
     quaternion_data,
-    sampling_rate
+    timestamp_data
     ):
     """
     Computes velocity and position from normalized acceleration data and quaternion data
@@ -139,6 +75,10 @@ def compute_velocity_position(
     quaternion_data: ndarray(N,4)
         Four point quaternion data
 
+    timestamps: ndarray(N,1)
+        timestamp
+
+
     Returns
     ---------
 
@@ -146,16 +86,39 @@ def compute_velocity_position(
     Raises an exception otherwise.
 
     """
+    initial_position = np.zeros(3)
+    mean_timestamp_delta = timestamp_data.diff().dropna().mean()
+
 
     normalized_acc_data = normalize_acceleration_data_quat(raw_acceleration_data, quaternion_data)
 
     velocity_data = np.nan*np.ones_like(normalized_acc_data)
     position_data = np.nan*np.ones_like(normalized_acc_data)
 
-    initial_position = np.zeros(3)
-
     for ii in range(normalized_acc_data.shape[1]):
-        velocity_data[:,ii] = scipy.integrate.cumtrapz(normalized_acc_data[:,ii], dx=1./sampling_rate, initial=0)
-        position_data[:,ii] = scipy.integrate.cumtrapz(velocity_data[:,ii], dx=1./sampling_rate, initial=initial_position[ii])
-
+        dt = timestamp_data.diff().fillna(0)
+        # velocity_data[:,ii] = (raw_acceleration_data.iloc[:, ii] * dt).cumsum()
+        velocity_data[:,ii] = scipy.integrate.cumtrapz(normalized_acc_data[:,ii], dx=mean_timestamp_delta, initial=0)
+        # position_data[:,ii] = (velocity_data[:, ii] * dt).cumsum()
+        position_data[:,ii] = scipy.integrate.cumtrapz(velocity_data[:,ii], dx=mean_timestamp_delta, initial=initial_position[ii])
     return (velocity_data, position_data)
+
+
+
+
+
+    # normalized_acc_data = normalize_acceleration_data_quat(raw_acceleration_data, quaternion_data)
+
+    # velocity_data = np.nan*np.ones_like(normalized_acc_data)
+    # position_data = np.nan*np.ones_like(normalized_acc_data)
+
+    # mean_timestamp_delta = timestamp_data.diff().dropna().mean()
+
+    # initial_position = np.zeros(3)
+
+    # for ii in range(normalized_acc_data.shape[1]):
+    #     velocity_data[:,ii] = scipy.integrate.cumtrapz(normalized_acc_data[:,ii], dx=mean_timestamp_delta, initial=0)
+    #     position_data[:,ii] = scipy.integrate.cumtrapz(velocity_data[:,ii], dx=mean_timestamp_delta, initial=initial_position[ii])
+
+
+    # return (velocity_data, position_data)
